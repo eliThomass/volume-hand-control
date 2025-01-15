075d07f86d46bf974113efdf6a182521da6b929a
Eli Thomas <38369558+eliThomass@users.noreply.github.com>
2025-01-15 13:02:59 -0800

    added circles on index tip and thumb tip, these tips will be used to determine volume by distance

--- VolumeHandControl.py
+++ VolumeHandControl.py
@@ -4,8 +4,10 @@ import numpy as np
 import mediapipe as mp
 
 cap = cv.VideoCapture(0)
-cap.set(3, 640)
-cap.set(4, 480)
+image_width = 640
+image_height = 480
+cap.set(3, image_width)
+cap.set(4, image_height)
 pTime = 0
 
 mp_hands = mp.solutions.hands
@@ -14,7 +16,7 @@ mp_drawing = mp.solutions.drawing_utils
 with mp_hands.Hands(
     static_image_mode=False,
     max_num_hands=2,
-    min_detection_confidence=0.5,
+    min_detection_confidence=0.7,
     min_tracking_confidence=0.5) as hands:
         while True:
             ret, frame = cap.read()
@@ -23,9 +25,18 @@ with mp_hands.Hands(
             results = hands.process(frame)
 
             if results.multi_hand_landmarks:
-              for hand_landmarks in results.multi_hand_landmarks:
-                 mp_drawing.draw_landmarks(
-                    frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+                for hand_landmarks in results.multi_hand_landmarks:
+                    mp_drawing.draw_landmarks(
+                        frame, hand_landmarks, mp_hands.HAND_CONNECTIONS)
+                    x_index = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].x * image_width
+                    y_index = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y * image_height
+                    x_thumb = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].x * image_width
+                    y_thumb = hand_landmarks.landmark[mp_hands.HandLandmark.THUMB_TIP].y * image_height
+
+                    cv.circle(frame, (int(x_index), int(y_index)), 15, (255,0,255), -1)
+                    cv.circle(frame, (int(x_thumb), int(y_thumb)), 15, (255,0,255), -1)
+
+                    
 
             cTime = time.time()
             fps = 1/(cTime-pTime)
